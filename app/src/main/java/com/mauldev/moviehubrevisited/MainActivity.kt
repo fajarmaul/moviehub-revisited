d8e2d7701c94913cd01ceead6db4274d358f57c0
Fajar <fajar@sweetescape.com>
2020-04-28 14:48:40 +0700

    Initial push and declaring library used

---
+++ app/src/main/java/com/mauldev/moviehubrevisited/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mauldev.moviehubrevisited
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
